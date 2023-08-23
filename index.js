3a2832b7e5cdbbfac4dd80f3f2887bb1d6284244
K44aue <128631337+K44aue@users.noreply.github.com>
2023-08-23 16:08:01 -0300

    arquivos inicial comit1

---
+++ index.js
@@ -0,0 +1,6 @@
+function acenderLampada() {
+    document.getElementById('lampada').src ="./img/lamp2.png";
+}
+function apagarLampada() {
+    document.getElementById('lampada').src ="./img/lamp1.png";
+}
